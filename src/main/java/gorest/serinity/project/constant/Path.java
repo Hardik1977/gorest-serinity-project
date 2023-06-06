0f51d51101aebda063f71c8c6d3444fb2dc18f75
hardik1977 <hardik77@yahoo.com>
2023-06-06 18:12:47 +0100

    gorest serenity project wk18 hb

---
+++ src/main/java/gorest/serinity/project/constant/Path.java
@@ -0,0 +1,7 @@
+package gorest.serinity.project.constant;
+
+public class Path {
+
+    public static final String PATH="/users";
+
+}
---
+++ src/main/java/gorest/serinity/project/gorestappinfo/UserSteps.java
@@ -0,0 +1,109 @@
+package gorest.serinity.project.gorestappinfo;
+
+import gorest.serinity.project.constant.EndPoints;
+import gorest.serinity.project.models.UserPojo;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Step;
+
+import java.util.HashMap;
+
+public class UserSteps {
+
+    @Step("Creating User with name : {0}, email: {1}, gender: {2}, status: {3}")
+    public ValidatableResponse createUser(String name,
+                                          String email,
+                                          String gender,
+                                          String status) {
+        UserPojo userPojo = new UserPojo();
+        userPojo.setName(name);
+        userPojo.setEmail(email);
+        userPojo.setGender(gender);
+        userPojo.setStatus(status);
+
+        return SerenityRest.given().log().all()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer 25fb8bbd4e5fc3d6ae066205e102ec57d5a1341d5e3f20f4dd96aea34ad8b805")
+                .body(userPojo)
+                .when()
+                .post(EndPoints.CREATE_USER)
+                .then();
+//        @Step("Creating user with name : {0}, email : {1}, gender : {2}, status : {3}")
+//        public ValidatableResponse createUser(String name, String email, String gender, String status) {
+//            UserPojo userPojo = UserPojo.getUserPojo(name, email, gender, status);
+//
+//
+//            return SerenityRest.given()
+//                    .contentType(ContentType.JSON)
+//                    .header("Accept", "application/json")
+//                    .header("Authorization", "Bearer 25fb8bbd4e5fc3d6ae066205e102ec57d5a1341d5e3f20f4dd96aea34ad8b805")
+//                    .when()
+//                    .body(userPojo)
+//                    .post(EndPoints.GET_ALL_USERS)
+//                    .then();
+//    }
+
+
+    }
+
+    @Step("Getting the product information with id:{0}")
+    public HashMap<String, Object> getProductInfoAddedByID(String name) {
+        String p1 = "findAll{it.name == '";
+        String p2 = "'}.get(0)";
+        return SerenityRest.given().log().all()
+                .header("Authorization", "Bearer 25fb8bbd4e5fc3d6ae066205e102ec57d5a1341d5e3f20f4dd96aea34ad8b805")
+                .when()
+                .get(EndPoints.GET_ALL_USER)
+                .then()
+                .statusCode(200)
+                .extract()
+                .path(p1 + name + p2);
+
+}
+
+    @Step("Updating users information with name:{0},email:{1},gender:{2},status:{3}")
+    public ValidatableResponse updateUser(int id,
+                                          String name,
+                                          String email,
+                                          String gender,
+                                          String status) {
+
+        UserPojo userPojo = new UserPojo();
+        userPojo.setName(name);
+        userPojo.setEmail(email);
+        userPojo.setGender(gender);
+        userPojo.setStatus(status);
+
+        return SerenityRest.given().log().all()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer 25fb8bbd4e5fc3d6ae066205e102ec57d5a1341d5e3f20f4dd96aea34ad8b805")
+                .pathParam("id",id)
+                .body(userPojo)
+                .when()
+                .put(EndPoints.UPDATE_USER_BY_ID)
+                .then();
+    }
+    @Step("Deleting user information with id: {0}")
+
+    public ValidatableResponse deleteUser(int id){
+        return  SerenityRest.given().log().all()
+                .header("Authorization", "Bearer 25fb8bbd4e5fc3d6ae066205e102ec57d5a1341d5e3f20f4dd96aea34ad8b805")
+                .pathParam("id",id)
+                .when()
+                .delete(EndPoints.DELETE_USER_BY_ID)
+                .then();
+    }
+    @Step("Getting student information with studentId: {0}")
+    public ValidatableResponse getUserById(int id){
+        return  SerenityRest.given().log().all()
+                .header("Authorization", "Bearer 25fb8bbd4e5fc3d6ae066205e102ec57d5a1341d5e3f20f4dd96aea34ad8b805")
+                .pathParam("id",id)
+                .when()
+                .get(EndPoints.GET_USER_BY_ID)
+                .then()
+                .statusCode(404);
+    }
+}
+
+
